bd1fc052cdef79b533506dfd5fb3ddb438041668
FRAALES <alessandrofranco1992@gmail.com>
2023-06-12 16:19:23 +0200

    stuck on import PageHeader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<!-- Bisogna cambiare prima di tutto le cose della composition API, dato che usiamo l'options API -->
+<script>
+import PageHeader from './components/PageHeader.vue';
+
+export default {
+  components: {
+    PageHeader,
+  }
+}
 </script>
 
 <template>
@@ -21,9 +28,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
-<script setup>
+<script>
+// Qui va il codice JavaScript
 import { ref } from 'vue'
 
 defineProps({
@@ -9,6 +10,7 @@ const count = ref(0)
 </script>
 
 <template>
+  <!-- Il template si divide in sections -->
   <h1>{{ msg }}</h1>
 
   <div class="card">
@@ -21,9 +23,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
@@ -34,6 +35,7 @@ const count = ref(0)
 </template>
 
 <style scoped>
+/* Questa parte di css è relativa solo a questo componente  */
 .read-the-docs {
   color: #888;
 }
